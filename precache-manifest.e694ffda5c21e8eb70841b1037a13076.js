ce80aab6977a642efa254a859209d1ca8f23540f
Ai Qiming <aiqiming199601@gmail.com>
2020-06-01 17:36:52 +0800

    Updates

---
+++ precache-manifest.e694ffda5c21e8eb70841b1037a13076.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d1a2af75223eb6a519fd485c7a692e",
+    "url": "/hysia_mm_demo/index.html"
+  },
+  {
+    "revision": "8de37b40f8bd2bacce1e",
+    "url": "/hysia_mm_demo/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "97cc09f322e93166c2e0",
+    "url": "/hysia_mm_demo/static/css/main.676d089b.chunk.css"
+  },
+  {
+    "revision": "8de37b40f8bd2bacce1e",
+    "url": "/hysia_mm_demo/static/js/2.b4e75ae4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hysia_mm_demo/static/js/2.b4e75ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cc09f322e93166c2e0",
+    "url": "/hysia_mm_demo/static/js/main.18bf72fb.chunk.js"
+  },
+  {
+    "revision": "5c44e0c0620ba7836e98",
+    "url": "/hysia_mm_demo/static/js/runtime-main.3fe28941.js"
+  },
+  {
+    "revision": "7d7247d5b307952a30f331aa504138dd",
+    "url": "/hysia_mm_demo/static/media/1-upload-video.7d7247d5.mp4"
+  },
+  {
+    "revision": "ec4dbfb367afce79a07d36da83911828",
+    "url": "/hysia_mm_demo/static/media/2-analysis-result.ec4dbfb3.mp4"
+  },
+  {
+    "revision": "baa29bb06db99dc9ed192926d0df87ec",
+    "url": "/hysia_mm_demo/static/media/3-search-scene.baa29bb0.mp4"
+  },
+  {
+    "revision": "68b05d305f102fca195eede959da0398",
+    "url": "/hysia_mm_demo/static/media/4-insert-product.68b05d30.mp4"
+  },
+  {
+    "revision": "3f7c37f14f85343bdd0b2a8be5bf01ad",
+    "url": "/hysia_mm_demo/static/media/5-ads-display.3f7c37f1.mp4"
+  },
+  {
+    "revision": "c485e1ff3b59b09e38d845fcaae5f64f",
+    "url": "/hysia_mm_demo/static/media/product.c485e1ff.mp4"
+  }
+]);
